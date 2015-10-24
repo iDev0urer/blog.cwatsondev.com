879a416dfee56846f6868780e73991213f5ecf54
Chris Watson <cawatson1993@gmail.com>
2015-10-24 15:04:33 -0700

    Add AASM states to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,22 @@
 class Post < ActiveRecord::Base
+  include AASM
   include Redcarpeted
   redcarpet :content
 
   acts_as_taggable
 
+  aasm do
+    state :draft, initial: true
+    state :pending
+    state :published
+
+    event :publish do
+      transitions from: [:draft, :pending], to: :published
+    end
+
+    event :draft do
+      transitions from: [:published, :pending], to: :draft
+    end
+  end
 
 end
